557eecced48b0bb03a9a0928081013caf41f73f3
jersonjanke <jersonjanke@gmail.com>
2016-04-30 10:56:03 -0300

    Ajustment values scope

--- app/app.module.js
+++ app/app.module.js
@@ -1,251 +1,268 @@
 /*
-* Projeto: Previsão do Tempo
-* Data de criação: 15/01/2016
-* Data do término : 20/01/2016
-* Programador: Jerson Janke
-* GitHub: https://github.com/jersonjanke/previsao-tempo
-*/
+ * Projeto: Previsão do Tempo
+ * Data de criação: 15/01/2016
+ * Data do término : 20/01/2016
+ * Programador: Jerson Janke
+ * GitHub: https://github.com/jersonjanke/previsao-tempo
+ */
 var app = angular.module("minhaPrevisao", []);
 
 //Verifica se tem favorito salvo
-if ((localStorage.estadoCache == undefined ) || (localStorage.cidadeCache == undefined )){
-  estado = "SC";
-  cidade = "Blumenau";
+if ((localStorage.estadoCache == undefined ) || (localStorage.cidadeCache == undefined )) {
+    estado = "SC";
+    cidade = "Blumenau";
 } else {
-  estado = localStorage.estadoCache;
-  cidade = localStorage.cidadeCache;
+    estado = localStorage.estadoCache;
+    cidade = localStorage.cidadeCache;
 }
 
 // Carrega dados
-app.controller("myCtrlTemp", function($scope, $http){
-  //Atualiza dados iniciais
-  $http.get("http://developers.agenciaideias.com.br/tempo/json/"+cidade+"-"+estado).
-  success(function(response){
-    $scope.carregaPrevisao(response,cidade,estado);
-  }).error(function(response){
-    $scope.msg = "Não existem previsão do tempo para está cidade e estado";
-    $('#myModal').modal('show');
-  });
-
-  //**** METODO ****
-  //Nova consulta de previssão
-  $scope.consultaPrevisao = function(){
-    if(($scope.cidade == undefined)||($scope.estado == undefined)){
-      $scope.msg = "Informe a cidade e estado para consultar a previsão do tempo.";
-      $('#myModal').modal('show');
-    }
-    else{
-      $scope.carregaDados( $scope.cidade,$scope.estado);
-      $scope.carregando = false;
-    }
-  }
-
-  //**** METODO ****
-  //Nova consulta ao clicar no botão consultar
-  $scope.carregaDados = function(cidade,estado){
-    $http.get("http://developers.agenciaideias.com.br/tempo/json/"+cidade+"-"+estado).
-    success(function(response){
-      $scope.carregaPrevisao(response,cidade,estado);
-    }).error(function(response){
-      $scope.msg = "Não existem previsão do tempo para está cidade e estado";
-      $('#myModal').modal('show');
+app.controller("myCtrlTemp", function ($scope, $http) {
+    //Atualiza dados iniciais
+    $http.get("http://developers.agenciaideias.com.br/tempo/json/" + cidade + "-" + estado).success(function (response) {
+        $scope.carregaPrevisao(response, cidade, estado);
+    }).error(function (response) {
+        $scope.msg = "Não existem previsão do tempo para está cidade e estado";
+        $('#myModal').modal('show');
     });
-  }
 
-  //**** METODO ****
-  // Atualiza Box Máximo e Mínima
-  $scope.atualizaPrevisoes = function($scope, lista){
-    var maxima = 0;
-    var minima = 99;
-    var dataMaxima;
-    var dataMinima;
-
-    for (var i = 0; i < lista.length; i++) {
-      var dia = lista[i];
-      // Temperatura Máxima
-      if(dia.temperatura_max > maxima) {
-        maxima = dia.temperatura_max;
-        dataMaxima = dia.data;
-      }
-      // Temperatura Mínima
-      if(dia.temperatura_min < minima) {
-        minima = dia.temperatura_min;
-        dataMinima = dia.data;
-      }
-    }
-    $scope.max = maxima;
-    $scope.dtMax = dataMaxima;
-    $scope.min = minima;
-    $scope.dtMin = dataMinima;
-  }
-
-  //**** METODO ****
-  // Atualiza Box Recomendação verificando se
-  // no proximos dias terá final de semnana e temperatura for > 25
-  $scope.recomendacoes = function($scope, lista){
-
-    //Limpa variavesi
-    $scope.recomendacaoPositivo = "";
-    $scope.recomendacaoNegativo = "";
-    $scope.finalSemana = false;
-
-    //Se dia atual for sábado
-    if ($scope.copiaDiaSemana((lista[2].data)) == 'Sábado'){
-      temperatura = lista[3].temperatura_max
-      if(temperatura >= 25){
-        $scope.recomendacaoPositivo = true;
-        $scope.finalSemana = true;
-      } else {
-        $scope.recomendacaoNegativo = true;
-        $scope.finalSemana = true;
-      }
-    }
-
-    for (var i = 3; i < 7; i++) {
-      var dia = "";
-      var temperatura = 0;
-
-      dia = lista[i].data;
-      dia = $scope.copiaDiaSemana(dia);
-
-      if(dia == "Sábado"){
-        temperatura = lista[i].temperatura_max;
-        console.log(temperatura);
-        if(temperatura >= 25){
-          $scope.recomendacaoPositivo = true;
-          $scope.finalSemana = true;
-        } else {
-          $scope.recomendacaoNegativo = true;
-          $scope.finalSemana = true;
+    //**** METODO ****
+    //Nova consulta de previssão
+    $scope.consultaPrevisao = function () {
+        if (($scope.cidade == undefined) || ($scope.estado == undefined)) {
+            $scope.msg = "Informe a cidade e estado para consultar a previsão do tempo.";
+            $('#myModal').modal('show');
         }
-      }
-    }
-
-    //Se não houver final de semana exibe mensagem.
-    if($scope.finalSemana == false){
-      $scope.semFinal = true;
-    }
-  }
-
-  //**** METODO ****
-  //Carrega Gráfico
-  $scope.carregaGrafico = function($scope, lista){
-    var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
-    var areaChart = new Chart(areaChartCanvas);
-
-    var areaChartData = {
-      labels: [$scope.copiaDiaSemana(lista[2].data), $scope.copiaDiaSemana(lista[3].data),$scope.copiaDiaSemana(lista[4].data), $scope.copiaDiaSemana(lista[5].data), $scope.copiaDiaSemana(lista[6].data)],
-      datasets: [
-        {
-          label: "Electronics",
-          fillColor: "rgba(210, 214, 222, 1)",
-          strokeColor: "rgba(210, 214, 222, 1)",
-          pointColor: "rgba(210, 214, 222, 1)",
-          pointStrokeColor: "#c1c7d1",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [lista[2].temperatura_max, lista[3].temperatura_max,lista[4].temperatura_max, lista[5].temperatura_max, lista[6].temperatura_max] //Temperatura Máxima
-        },
-        {
-          label: "Digital Goods",
-          fillColor: "rgba(60,141,188,0.9)",
-          strokeColor: "rgba(60,141,188,0.8)",
-          pointColor: "#3b8bba",
-          pointStrokeColor: "rgba(60,141,188,1)",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(60,141,188,1)",
-          data: [lista[2].temperatura_min, lista[3].temperatura_min,lista[4].temperatura_min, lista[5].temperatura_min, lista[6].temperatura_min] //Temperatura Mínima
+        else {
+            $scope.carregaDados($scope.cidade, $scope.estado);
+            $scope.carregando = false;
         }
-      ]
-    };
-
-    var areaChartOptions = {
-      //Boolean - If we should show the scale at all
-      showScale: true,
-      //Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines: true,
-      //String - Colour of the grid lines
-      scaleGridLineColor: "rgba(0,0,0,.05)",
-      //Number - Width of the grid lines
-      scaleGridLineWidth: 1,
-      //Boolean - Whether to show horizontal lines (except X axis)
-      scaleShowHorizontalLines: true,
-      //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines: true,
-      //Boolean - Whether the line is curved between points
-      bezierCurve: true,
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension: 0.3,
-      //Boolean - Whether to show a dot for each point
-      pointDot: true,
-      //Number - Radius of each point dot in pixels
-      pointDotRadius: 4,
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth: 1,
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius: 20,
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke: true,
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth: 2,
-      //Boolean - Whether to fill the dataset with a color
-      datasetFill: true,
-      //String - A legend template
-      legendTemplate: "<ul class='chart-legend'><li><span style='background-color: #c1c7d1'></span>label1</li><li><span style='background-color: #3b8bba'></span>label1</li></ul>",
-      //legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-      //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-      maintainAspectRatio: true,
-      //Boolean - whether to make the chart responsive to window resizing
-      responsive: true
-    };
-    //Create the line chart
-    areaChart.Line(areaChartData, areaChartOptions);
-  } // Fim Gráfico
-
-  //**** METODO ****
-  // Salvar Favoritos
-  $scope.salvaFavorito = function(cid,est){
-    if((cid == undefined) || (est == undefined)){
-      $scope.msg = "Informar a cidade e estado para salvar favorito.";
-      $('#myModal').modal('show');
     }
-    else{
-      localStorage.cidadeCache = cid;
-      localStorage.estadoCache = est;
-      $scope.msg = 'Favorito salvo com sucesso.';
-      $('#myModal').modal('show');
-    }
-  }
 
-  //**** METODO ****
-  // Copia somente o nome da semana
-  $scope.copiaDiaSemana = function(data){
-    var dia = "";
-    for (var i = 0; i < data.length; i++) {
-      var str = "";
-      var str = (data.charAt(i));
-      dia = dia + str;
-      if(str == ' ')
-      break;
+    //**** METODO ****
+    //Nova consulta ao clicar no botão consultar
+    $scope.carregaDados = function (cidade, estado) {
+        $http.get("http://developers.agenciaideias.com.br/tempo/json/" + cidade + "-" + estado).success(function (response) {
+            $scope.carregaPrevisao(response, cidade, estado);
+        }).error(function (response) {
+            $scope.msg = "Não existem previsão do tempo para está cidade e estado";
+            $('#myModal').modal('show');
+        });
     }
-    dia = dia.trim();
-    return dia;
-  }
 
-  //**** METODO ****
-  $scope.carregaPrevisao = function(previsao,cidade,estado){
-    obj = $scope.myData = previsao;
-    lista = $.map(obj, function(el) { return el });
-    $scope.atualizaPrevisoes($scope,lista);
-    $scope.recomendacoes($scope,lista);
-    $scope.carregaGrafico($scope,lista);
-    $scope.carregando = true;
-    $scope.cidadeAtual = cidade;
-    $scope.estadoAtual = estado;
-    $scope.imagem00 = $scope.myData.previsoes[0].imagem;
-    $scope.imagem01 = $scope.myData.previsoes[1].imagem;
-    $scope.imagem02 = $scope.myData.previsoes[2].imagem;
-    $scope.imagem03 = $scope.myData.previsoes[3].imagem;
-    $scope.imagem04 = $scope.myData.previsoes[4].imagem;
-  }
+    //**** METODO ****
+    // Atualiza Box Máximo e Mínima
+    $scope.atualizaPrevisoes = function ($scope, lista) {
+        var maxima = 0;
+        var minima = 99;
+        var dataMaxima;
+        var dataMinima;
+
+        for (var i = 0; i < lista.length; i++) {
+            var dia = lista[i];
+            // Temperatura Máxima
+            if (dia.temperatura_max > maxima) {
+                maxima = dia.temperatura_max;
+                dataMaxima = dia.data;
+            }
+            // Temperatura Mínima
+            if (dia.temperatura_min < minima) {
+                minima = dia.temperatura_min;
+                dataMinima = dia.data;
+            }
+        }
+        $scope.max = maxima;
+        $scope.dtMax = dataMaxima;
+        $scope.min = minima;
+        $scope.dtMin = dataMinima;
+    }
+
+    //**** METODO ****
+    // Atualiza Box Recomendação verificando se
+    // no proximos dias terá final de semnana e temperatura for > 25
+    $scope.recomendacoes = function ($scope, lista) {
+
+        //Limpa variavesi
+        $scope.recomendacaoPositivo = "";
+        $scope.recomendacaoNegativo = "";
+        $scope.finalSemana = false;
+
+        //Se dia atual for sábado
+        if ($scope.copiaDiaSemana((lista[2].data)) == 'Sábado') {
+            temperatura = lista[3].temperatura_max
+            if (temperatura >= 25) {
+                $scope.recomendacaoPositivo = true;
+                $scope.finalSemana = true;
+            } else {
+                $scope.recomendacaoNegativo = true;
+                $scope.finalSemana = true;
+            }
+        }
+
+        for (var i = 3; i < 7; i++) {
+            var dia = "";
+            var temperatura = 0;
+
+            dia = lista[i].data;
+            dia = $scope.copiaDiaSemana(dia);
+
+            if (dia == "Sábado") {
+                temperatura = lista[i].temperatura_max;
+                console.log(temperatura);
+                if (temperatura >= 25) {
+                    $scope.recomendacaoPositivo = true;
+                    $scope.finalSemana = true;
+                } else {
+                    $scope.recomendacaoNegativo = true;
+                    $scope.finalSemana = true;
+                }
+            }
+        }
+
+        //Se não houver final de semana exibe mensagem.
+        if ($scope.finalSemana == false) {
+            $scope.semFinal = true;
+        }
+    }
+
+    //**** METODO ****
+    //Carrega Gráfico
+    $scope.carregaGrafico = function ($scope, lista) {
+        var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+        var areaChart = new Chart(areaChartCanvas);
+
+        var areaChartData = {
+            labels: [$scope.copiaDiaSemana(lista[2].data), $scope.copiaDiaSemana(lista[3].data), $scope.copiaDiaSemana(lista[4].data), $scope.copiaDiaSemana(lista[5].data), $scope.copiaDiaSemana(lista[6].data)],
+            datasets: [
+                {
+                    label: "Electronics",
+                    fillColor: "rgba(210, 214, 222, 1)",
+                    strokeColor: "rgba(210, 214, 222, 1)",
+                    pointColor: "rgba(210, 214, 222, 1)",
+                    pointStrokeColor: "#c1c7d1",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [lista[2].temperatura_max, lista[3].temperatura_max, lista[4].temperatura_max, lista[5].temperatura_max, lista[6].temperatura_max] //Temperatura Máxima
+                },
+                {
+                    label: "Digital Goods",
+                    fillColor: "rgba(60,141,188,0.9)",
+                    strokeColor: "rgba(60,141,188,0.8)",
+                    pointColor: "#3b8bba",
+                    pointStrokeColor: "rgba(60,141,188,1)",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(60,141,188,1)",
+                    data: [lista[2].temperatura_min, lista[3].temperatura_min, lista[4].temperatura_min, lista[5].temperatura_min, lista[6].temperatura_min] //Temperatura Mínima
+                }
+            ]
+        };
+
+        var areaChartOptions = {
+            //Boolean - If we should show the scale at all
+            showScale: true,
+            //Boolean - Whether grid lines are shown across the chart
+            scaleShowGridLines: true,
+            //String - Colour of the grid lines
+            scaleGridLineColor: "rgba(0,0,0,.05)",
+            //Number - Width of the grid lines
+            scaleGridLineWidth: 1,
+            //Boolean - Whether to show horizontal lines (except X axis)
+            scaleShowHorizontalLines: true,
+            //Boolean - Whether to show vertical lines (except Y axis)
+            scaleShowVerticalLines: true,
+            //Boolean - Whether the line is curved between points
+            bezierCurve: true,
+            //Number - Tension of the bezier curve between points
+            bezierCurveTension: 0.3,
+            //Boolean - Whether to show a dot for each point
+            pointDot: true,
+            //Number - Radius of each point dot in pixels
+            pointDotRadius: 4,
+            //Number - Pixel width of point dot stroke
+            pointDotStrokeWidth: 1,
+            //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+            pointHitDetectionRadius: 20,
+            //Boolean - Whether to show a stroke for datasets
+            datasetStroke: true,
+            //Number - Pixel width of dataset stroke
+            datasetStrokeWidth: 2,
+            //Boolean - Whether to fill the dataset with a color
+            datasetFill: true,
+            //String - A legend template
+            legendTemplate: "<ul class='chart-legend'><li><span style='background-color: #c1c7d1'></span>label1</li><li><span style='background-color: #3b8bba'></span>label1</li></ul>",
+            //legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+            //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+            maintainAspectRatio: true,
+            //Boolean - whether to make the chart responsive to window resizing
+            responsive: true
+        };
+        //Create the line chart
+        areaChart.Line(areaChartData, areaChartOptions);
+    } // Fim Gráfico
+
+    //**** METODO ****
+    // Salvar Favoritos
+    $scope.salvaFavorito = function (cid, est) {
+        if ((cid == undefined) || (est == undefined)) {
+            $scope.msg = "Informar a cidade e estado para salvar favorito.";
+            $('#myModal').modal('show');
+        }
+        else {
+            localStorage.cidadeCache = cid;
+            localStorage.estadoCache = est;
+            $scope.msg = 'Favorito salvo com sucesso.';
+            $('#myModal').modal('show');
+        }
+    }
+
+    //**** METODO ****
+    // Copia somente o nome da semana
+    $scope.copiaDiaSemana = function (data) {
+        var dia = "";
+        for (var i = 0; i < data.length; i++) {
+            var str = "";
+            var str = (data.charAt(i));
+            dia = dia + str;
+            if (str == ' ')
+                break;
+        }
+        dia = dia.trim();
+        return dia;
+    }
+
+    //**** METODO ****
+    $scope.carregaPrevisao = function (previsao, cidade, estado) {
+        obj = $scope.myData = previsao;
+        lista = $.map(obj, function (el) {
+            return el
+        });
+        $scope.atualizaPrevisoes($scope, lista);
+        $scope.recomendacoes($scope, lista);
+        $scope.carregaGrafico($scope, lista);
+        $scope.carregando = true;
+        $scope.cidadeAtual = cidade;
+        $scope.estadoAtual = estado;
+
+        //Imagens
+        $scope.imagem00 = $scope.myData.previsoes[0].imagem;
+        $scope.imagem01 = $scope.myData.previsoes[1].imagem;
+        $scope.imagem02 = $scope.myData.previsoes[2].imagem;
+        $scope.imagem03 = $scope.myData.previsoes[3].imagem;
+        $scope.imagem04 = $scope.myData.previsoes[4].imagem;
+
+        //Temperaturas Minima
+        $scope.min00 = $scope.myData.previsoes[0].temperatura_min + ' °';
+        $scope.min01 = $scope.myData.previsoes[1].temperatura_min + ' °';
+        $scope.min02 = $scope.myData.previsoes[2].temperatura_min + ' °';
+        $scope.min03 = $scope.myData.previsoes[3].temperatura_min + ' °';
+        $scope.min04 = $scope.myData.previsoes[4].temperatura_min + ' °';
+
+        //Temperatura Maxima
+        $scope.max00 = $scope.myData.previsoes[0].temperatura_max + ' °';
+        $scope.max01 = $scope.myData.previsoes[1].temperatura_max + ' °';
+        $scope.max02 = $scope.myData.previsoes[2].temperatura_max + ' °';
+        $scope.max03 = $scope.myData.previsoes[3].temperatura_max + ' °';
+        $scope.max04 = $scope.myData.previsoes[4].temperatura_max + ' °';
+
+    }
 });
